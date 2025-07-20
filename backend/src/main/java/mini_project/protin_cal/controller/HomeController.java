8311474425bad90e9ad480a28e026f9a7e164102
takch02 <lim70kr38@gmail.com>
2025-07-20 18:50:20 +0900

    frontend 모듈 다운

--- backend/src/main/java/mini_project/protin_cal/controller/HomeController.java
+++ backend/src/main/java/mini_project/protin_cal/controller/HomeController.java
@@ -3,6 +3,10 @@ package mini_project.protin_cal.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 public class HomeController {
 
@@ -10,4 +14,5 @@ public class HomeController {
     public String home() {
         return "home";
     }
+
 }
--- backend/src/test/java/mini_project/protin_cal/ProtinCalApplicationTests.java
+++ backend/src/test/java/mini_project/protin_cal/ProtinCalApplicationTests.java
@@ -3,11 +3,16 @@ package mini_project.protin_cal;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootTest
 class ProtinCalApplicationTests {
 
     @Test
     void contextLoads() {
+
     }
 
 }
