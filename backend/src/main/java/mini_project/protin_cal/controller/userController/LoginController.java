8cb633cf55aaf2a78c67da5da46bfef585f88568
takch02 <lim70kr38@gmail.com>
2025-07-31 22:48:10 +0900

    feat : 백엔드 서버 URL 적용

--- backend/src/main/java/mini_project/protin_cal/controller/userController/LoginController.java
+++ backend/src/main/java/mini_project/protin_cal/controller/userController/LoginController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +22,7 @@ import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
+@CrossOrigin(origins = "http://ec2-15-165-16-46.ap-northeast-2.compute.amazonaws.com:3000")
 public class LoginController {
 
     @Autowired
