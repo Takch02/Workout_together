b03486dc75d9404a92961f5da1075e125b00777f
takch02 <lim70kr38@gmail.com>
2025-07-31 14:47:58 +0900

    feat : 백엔드 서버 URL 적용

--- backend/src/main/java/mini_project/protin_cal/config/WebConfig.java
+++ backend/src/main/java/mini_project/protin_cal/config/WebConfig.java
@@ -29,7 +29,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new SessionInterceptor())
                 .addPathPatterns("/**")
-                .excludePathPatterns("/favicon.ico","/user/login", "/user/join");
+                .excludePathPatterns("/favicon.ico","/user/login", "/user/join", "/main");
     }
 
 }
