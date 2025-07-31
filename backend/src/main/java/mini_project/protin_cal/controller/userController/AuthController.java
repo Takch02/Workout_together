aa5d42d05ebf013e9b4ff1e5092c4d01170c1321
takch02 <lim70kr38@gmail.com>
2025-07-31 22:16:15 +0900

    feat : 백엔드 서버 URL 적용

--- backend/src/main/java/mini_project/protin_cal/controller/userController/AuthController.java
+++ backend/src/main/java/mini_project/protin_cal/controller/userController/AuthController.java
@@ -4,6 +4,8 @@ import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,4 +21,10 @@ public class AuthController {
             return ResponseEntity.status(200).body("로그인 성공!");
         }
     }
+    @RequestMapping(value = "/user/login", method = RequestMethod.OPTIONS)
+    public ResponseEntity<?> handleOptions() {
+        return ResponseEntity.ok().build();
+    }
+
+
 }
--- backend/src/main/java/mini_project/protin_cal/controller/userController/LoginController.java
+++ backend/src/main/java/mini_project/protin_cal/controller/userController/LoginController.java
@@ -30,6 +30,7 @@ public class LoginController {
     /**
      * 로그인 로직. Valid에 걸리면 error를 보냄. 저장 완료되면 userDto 객체를 보냄. (id는 보내지 않음)
      */
+    /**
     @PostMapping("/user/login")
     public ResponseEntity<?> login(@Valid @RequestBody LoginDto loginDto, BindingResult bindingResult, HttpSession session) {
         log.info("로그인 로직 시작");
@@ -55,7 +56,7 @@ public class LoginController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
     }
-
+*/
     @PostMapping("/user/join")
     public ResponseEntity<?> join(@Valid @RequestBody UserDto userDto, BindingResult bindingResult) {
 
